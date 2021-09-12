c7610936f31a44ad65da49576e80697787106831
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-09-12 22:35:16 +0200

    reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
 extern crate clap;
-use std::{thread, time};
-use clap::{Arg, App};
+use clap::{App, Arg};
 use notify_rust::Notification;
+use std::{thread, time};
 
 fn main() {
     let matches = App::new("pomodoro")
-        .arg(Arg::with_name("time")
-             .short("t")
-             .long("time")
-             .value_name("TIME")
-             .help("set the time time"))
+        .arg(
+            Arg::with_name("time")
+                .short("t")
+                .long("time")
+                .value_name("TIME")
+                .help("set the time time"),
+        )
         .get_matches();
 
     let duration;
 
-    if let Ok(t) = 
-        matches.value_of("time")
-        .unwrap_or("2")
-        .parse::<u64>() {
-            duration = time::Duration::from_secs(t);
-        } else {
-            eprintln!("arg must be integer");
-            return;
-        }
+    if let Ok(t) = matches.value_of("time").unwrap_or("2").parse::<u64>() {
+        duration = time::Duration::from_secs(t);
+    } else {
+        eprintln!("arg must be integer");
+        return;
+    }
 
     thread::sleep(duration);
     println!("timer ended");
